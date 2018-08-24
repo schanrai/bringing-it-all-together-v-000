bc41aa0e95ed0c83b7e0cfcb19dac23215eaab14
Sushi Chanrai <sushi@pathworks.co>
2018-08-24 10:25:12 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -67,7 +67,6 @@ attr_reader :id
   def self.find_by_name(name)
     sql = "SELECT * FROM dogs WHERE name = ?"
     row = DB[:conn].execute(sql, name)[0]
-    #Student.new(result[0], result[1], result[2])
     self.new_from_db(row)
   end
 
