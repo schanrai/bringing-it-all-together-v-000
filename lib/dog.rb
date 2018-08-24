3d62a46e124e78720f28331f455c7558a73688b0
Sushi Chanrai <sushi@pathworks.co>
2018-08-24 10:23:37 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -64,4 +64,11 @@ attr_reader :id
   end
 
 
+  def self.find_by_name(name)
+    sql = "SELECT * FROM students WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+    Student.new(result[0], result[1], result[2])
+  end
+
+
 end
